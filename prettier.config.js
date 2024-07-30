1d8b587c18c3fa59c22ddbb2c407b8de198b5809
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-07-30 15:23:41 +0300

    initial commit 😎

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+export default {
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 100,
+  semi: false,
+  trailingComma: 'none',
+  jsxSingleQuote: true,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid'
+}
